fc0a48db3bd49e89ec52d831a7966d64126766df
chunlin-li <zhglclyx@163.com>
2015-10-23 20:24:42 +0800

    commit

--- libs/db/mongo.js
+++ libs/db/mongo.js
@@ -1,7 +1,6 @@
 'use strict';
 var co = require('co');
 var mongo = require('mongodb');
-var async = require('async');
 var EventEmitter = require('events');
 var util = require('util');
 var url = 'mongodb://localhost:27017/moduleTest';
@@ -10,10 +9,6 @@ var config = [
   ['db01', 'mongodb://localhost:27017/moduleTest', ['users', 'article']]
 ];
 var _dbs = {};
-
-var start = process.hrtime();
-var str = 'hellowould';
-this.str = str;
 
 function getDB(dbAlias) {
   if (!_dbs[dbAlias]) {
@@ -29,34 +24,33 @@ getDB.once = function (eventName, cb) {
   getDB._emitter.once(eventName, cb);
 };
 
-co(function* (done) {
-
+co(function* () {
+  // connect db
   let tmp = {};
   for (let i = 0; i < config.length; i ++ ) {
     tmp[config[i][0]] = mongo.connect(config[i][1]);
   }
   _dbs = yield tmp;
+
+  // setup collections
   config.forEach(function (item) {
     item[2].forEach(function (collName) {
       _dbs[item[0]][collName] = _dbs[item[0]].collection(collName);
     });
   });
-  done && done();
+
+  // finish
   getDB._emitter.emit('done');
 }).catch(function (err) {
-  getDB._emitter.listeners('error').length > 0 && getDB._emitter.emit('error', err);
-  console.error(err);
+  let listenerCount = getDB._emitter.listeners('error').length();
+  if (listenerCount > 0) {
+    getDB._emitter.emit('error', err);
+  } else {
+    throw err;
+  }
 });
-
-
-getDB.on('done', function () {
-  console.log('all done!');
-});
-getDB.on('error', function (err) {
-  console.error('receive error event!', err);
-});
+module.exports = getDB;
 
 
 
 
-//module.exports = getDB;
---
+++ libs/db/test.js
@@ -0,0 +1,75 @@
+'use strict';
+var assert = require('assert');
+var logger = require('../logger')('console');
+var db = require('./mongo');
+var co = require('co');
+var utils = require('util');
+
+describe('test mongo', function () {
+
+  before(function (done) {
+    db.on('done', function () { done(); });
+    db.on('error', function (err) { logger.error('err info : ${err}'); done(err); });
+  });
+
+
+  it('insert', function (done) {
+    co(function* () {
+
+      let doc = {
+        _id: 'testID',
+        name: 'Javascript',
+        timestamp: new Date()
+      };
+      var result = yield db('db01').users.insert(doc);
+      assert.equal(result.result.ok, 1);
+      done();
+
+    }).catch(onError.bind(null, done));
+  });
+
+  it('query', function (done) {
+    co(function* () {
+
+      var result = yield db('db01').users.find({_id:'testID'}).toArray();
+      result.forEach(function (item) {
+        assert.equal(item.name, 'Javascript');
+      });
+      done();
+
+    }).catch(onError.bind(null, done));
+  });
+
+  it('update', function (done) {
+    co(function* () {
+
+      var result = yield db('db01').users.update({_id:'testID'}, {'$set': {timestamp: new Date(1445593568726)}});
+      assert.equal(result.result.ok, 1);
+
+      var doc = yield db('db01').users.find({_id:'testID'}).limit(1).next();
+      assert.equal(doc.timestamp.getTime(), 1445593568726);
+
+      done();
+
+    }).catch(onError.bind(null, done));
+  });
+
+  it('delete', function (done) {
+    co(function* () {
+
+      var result = yield db('db01').users.remove({_id:'testID'});
+      assert.equal(result.result.ok, 1);
+
+      var count = yield db('db01').users.count({_id:'testID'});
+      assert.equal(count, 0);
+      done();
+
+    }).catch(onError.bind(null, done));
+  });
+
+  function onError(done, err) {
+    logger.error('Error Catched: ' + err.stack);
+    done(err);
+  }
+
+});
